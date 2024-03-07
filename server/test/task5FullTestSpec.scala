bd4758531a0cc4224c3d576b14d40c001bdad876
Noah Wenig <nwenig@trinity.edu>
2024-03-06 22:35:07 -0600

    Guice not wanting to compile :(

--- server/test/task5FullTestSpec.scala
+++ server/test/task5FullTestSpec.scala
@@ -6,6 +6,11 @@ import org.scalatestplus.play.HtmlUnitFactory
 
 class task5FullTestSpec extends PlaySpec with GuiceOneServerPerSuite with OneBrowserPerSuite with HtmlUnitFactory{
     "Task 5 message board" must{
-        
+        "login and access functions" in {
+            go to s"http://localhost:$port/task5"
+            pageTitle mustBe "Task 5 login"
+            find(cssSelector("h2")).isEmpty mustBe false 
+            //click on "loginUsername" 
+        }
     }
 }
