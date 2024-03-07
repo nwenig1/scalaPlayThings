f045909a09a6c8d9671b7a5c2208119ccaa5c2ff
Noah Wenig <nwenig@trinity.edu>
2024-03-06 18:01:13 -0600

    Started selenium, added ids to forms in login

---
+++ server/test/task5FullTestSpec.scala
@@ -0,0 +1,11 @@
+import org.scalatestplus.play.PlaySpec
+import org.scalatestplus.play.guice.GuiceOneServerPerSuite
+import org.scalatestplus.play.OneBrowserPerSuite
+import org.scalatestplus.play.HtmlUnitFactory
+
+
+class task5FullTestSpec extends PlaySpec with GuiceOneServerPerSuite with OneBrowserPerSuite with HtmlUnitFactory{
+    "Task 5 message board" must{
+        
+    }
+}
