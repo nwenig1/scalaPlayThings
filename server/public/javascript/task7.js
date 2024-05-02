1a5d5cba85fc70bb2619f1cefdcf8bbb73422d27
Noah Wenig <nwenig@trinity.edu>
2024-05-02 12:14:59 -0500

    removed prints

--- server/public/javascript/task7.js
+++ server/public/javascript/task7.js
@@ -22,20 +22,14 @@ let myself = {color: "black", posX: width/2, posY: height/2};
 let allSprites = [];
 document.addEventListener("keydown", function(event){
      if(event.key === "ArrowUp"){
-        console.log("up arrow pressed");
          socket.send([myself.posX, myself.posY -= 10])
      } else if(event.key === "ArrowLeft"){
-        console.log("left arrow pressed"); 
         socket.send([myself.posX-=10, myself.posY])
-     } else if(event.key === "ArrowRight"){
-        console.log("right arrow pressed"); 
+     } else if(event.key === "ArrowRight"){ 
         socket.send([myself.posX+=10, myself.posY])
      }else if(event.key === "ArrowDown"){
-        console.log("down arrow pressed"); 
         socket.send([myself.posX, myself.posY += 10])
-
      }
-
 }); 
 
 socket.onopen = (event) => socket.send([myself.posX, myself.posY]); 
@@ -43,7 +37,7 @@ socket.onopen = (event) => socket.send([myself.posX, myself.posY]);
 
 socket.onmessage = (event) =>{
 const actualData = JSON.parse(event.data.replaceAll('"','')); 
-console.log(actualData)
+
 redrawRectangles(actualData)
 
 
@@ -53,13 +47,9 @@ function redrawRectangles(locations){
    ctx.clearRect(0, 0, canvas.width, canvas.height)
    for(let i=0; i< locations.length; i++){
 
-     
-     // console.log("Actual coordinate is: " + coordinates);
       ctx.beginPath(); 
       ctx.fillStyle="black"; 
       ctx.stroke(); 
-      console.log(" x is " + locations[i][0]); 
-      console.log("y is " + locations[i][1]);
       ctx.fillRect(locations[i][0], locations[i][1], 50, 50)
       ctx.closePath(); 
    }
