3d56cf2a5a4ee46eab4c3af4f2c84865f7c01324
Noah Wenig <nwenig@trinity.edu>
2024-05-05 20:37:47 -0500

    added colors for funsies

--- client/src/main/scala/edu/trinity/videoquizreact/Task11.scala
+++ client/src/main/scala/edu/trinity/videoquizreact/Task11.scala
@@ -24,6 +24,7 @@ object Task11 {
     implicit val circlereads: play.api.libs.json.Reads[shared.Circle] = Json.reads[Circle]
     implicit val drawableReads: play.api.libs.json.Reads[shared.Drawable] = Json.reads[Drawable]
     var shape = "square" //starts off as square initially 
+    var color= "black"
     var linePoint1 : Option[(Int, Int)] = None
     var socketRoute = document.getElementById("ws-route").asInstanceOf[html.Input].value
     var socket = new dom.WebSocket(socketRoute.replace("http", "ws"));
@@ -47,17 +48,31 @@ object Task11 {
         val squareButton = document.getElementById("drawSquare").asInstanceOf[html.Button]
         squareButton.addEventListener("click", { (e0: dom.Event) =>
             shape = "square"
-           println("shape changed to " + shape)
         })
         val circleButton = document.getElementById("drawCircle").asInstanceOf[html.Button]
         circleButton.addEventListener("click", {(e0: dom.Event) =>
         shape = "circle"
-        println("shape changed to " + shape)
         })
          val lineButton = document.getElementById("drawLine").asInstanceOf[html.Button]
         lineButton.addEventListener("click", {(e0:dom.Event) =>
             shape="line"
-        println("shape changed to " + shape)
+        })
+        //adds event listeners for colors
+        val blackButton = document.getElementById("black").asInstanceOf[html.Button]
+        blackButton.addEventListener("click", {(e0: dom.Event) =>
+            color = "black"
+        })
+        val blueButton = document.getElementById("blue").asInstanceOf[html.Button]
+        blueButton.addEventListener("click", {(e0: dom.Event) =>
+            color = "blue"
+        })
+        val redButton = document.getElementById("red").asInstanceOf[html.Button]
+        redButton.addEventListener("click", {(e0: dom.Event) =>
+            color = "red"
+        })
+        val orangeButton = document.getElementById("orange").asInstanceOf[html.Button]
+        orangeButton.addEventListener("click", {(e0: dom.Event) =>
+            color = "orange"
         })
 
         //determines which new shape to send to the server
@@ -66,12 +81,12 @@ object Task11 {
         val e = e0.asInstanceOf[dom.MouseEvent]
         val xyTup = getCursorPosition(canvas, e)
         if(shape == "square"){
-            socket.send(Json.toJson(new Rectangle(xyTup._1, xyTup._2)).toString())
+            socket.send(Json.toJson(new Rectangle(xyTup._1-25, xyTup._2-25, color)).toString())
         }else if(shape == "line"){
             linePoint1 match {
                 //if first location has been clicked, send it along with the newly clicked 2nd point
                 case Some(linePoint) => { 
-                    socket.send(Json.toJson(new Line(linePoint._1, linePoint._2, xyTup._1, xyTup._2)).toString())
+                    socket.send(Json.toJson(new Line(linePoint._1, linePoint._2, xyTup._1, xyTup._2, color)).toString())
                     //resets the first line point for future lines
                     linePoint1 = None
                 }
@@ -81,7 +96,7 @@ object Task11 {
                 }
             }
         } else if(shape == "circle"){
-            socket.send(Json.toJson(new Circle(xyTup._1, xyTup._2, 50)).toString())
+            socket.send(Json.toJson(new Circle(xyTup._1, xyTup._2, 50, color)).toString())
         }    
     })
 }   
@@ -90,30 +105,31 @@ object Task11 {
         val canvas = document.getElementById("canvas").asInstanceOf[html.Canvas]
         drawings.map { drawing =>
             drawing match {
-                case Rectangle(x, y) => drawRect(canvas, x, y)
-                case Circle(cX, cY, radius) => drawCircle(canvas, cX, cY, radius)
-                case Line(x1, y1, x2, y2) => drawLine(canvas, x1, y1, x2, y2)
+                case Rectangle(x, y, color) => drawRect(canvas, x, y, color)
+                case Circle(cX, cY, radius, color) => drawCircle(canvas, cX, cY, radius, color)
+                case Line(x1, y1, x2, y2, color) => drawLine(canvas, x1, y1, x2, y2, color)
             }
         }
     }
-    def drawRect(canvas: html.Canvas, x: Int, y: Int): Unit = {
+    def drawRect(canvas: html.Canvas, x: Int, y: Int, color: String): Unit = {
         val ctx=canvas.getContext("2d")
+        ctx.fillStyle = color
         ctx.fillRect(x, y, 50, 50)
     }
-    def drawCircle(canvas: html.Canvas, cX: Int, cY: Int,radius: Double): Unit = {
+    def drawCircle(canvas: html.Canvas, cX: Int, cY: Int,radius: Double, color: String): Unit = {
         val ctx=canvas.getContext("2d")
         ctx.beginPath()
         ctx.arc(cX, cY, radius, 0, 2 * Math.PI) // x, y, radius, startAngle, endAngle
-        ctx.strokeStyle = "black"
+        ctx.strokeStyle = color
         ctx.lineWidth = 2
         ctx.stroke()
     }
-    def drawLine(canvas: html.Canvas, x1: Int, y1: Int, x2: Int, y2: Int){
+    def drawLine(canvas: html.Canvas, x1: Int, y1: Int, x2: Int, y2: Int, color:String ){
         val ctx=canvas.getContext("2d")
         ctx.beginPath()
         ctx.moveTo(x1, y1) 
         ctx.lineTo(x2, y2) 
-        ctx.strokeStyle = "black"
+        ctx.strokeStyle = color
         ctx.lineWidth = 2
         ctx.stroke()
     }
--- shared/src/main/scala/shared/Drawable.scala
+++ shared/src/main/scala/shared/Drawable.scala
@@ -2,10 +2,9 @@ package shared
 
 sealed trait Drawable 
 
-case class Rectangle(x: Int, y:Int) extends Drawable
-case class Line(x1:Int, y1:Int, x2:Int, y2:Int) extends Drawable
-
-case class Circle(cX: Int, cY: Int, radius: Double) extends Drawable
+case class Rectangle(x: Int, y:Int, color: String) extends Drawable
+case class Line(x1:Int, y1:Int, x2:Int, y2:Int, color: String) extends Drawable
+case class Circle(cX: Int, cY: Int, radius: Double, color: String) extends Drawable
 
 
 
